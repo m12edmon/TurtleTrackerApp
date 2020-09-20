c960bc32136c06ef10a5bb10a9f1f38189e3a4f3
m12edmon <mee28@duke.edu>
2020-09-20 15:29:47 -0400

    Created two dictionaries for iteration

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,6 +21,10 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+#Create two empty dictionary objects
+date_dict = {}
+coord_dict = {}
+
 #Iterate through all lines in the linelist
 for lineString in line_list:
     if lineString[0] in ("#", "u"): continue
@@ -37,3 +41,5 @@ for lineString in line_list:
     
     #Print the Location of Sara
     print(f'Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}')
+    date_dict[record_id] = obs_date
+    coord_dict[record_id] = (obs_lat,obs_lon)
