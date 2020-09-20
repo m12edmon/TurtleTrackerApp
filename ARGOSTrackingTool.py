91bac9cd08344224ec554e82c0c953a90a9138d2
m12edmon <mee28@duke.edu>
2020-09-20 14:46:26 -0400

    Read data in from file

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,18 +9,30 @@
 # Date:   Fall 2020
 #--------------------------------------------------------------
 
+#Create a variable pointing to the data file
+file_name = './Data/raw/sara.txt'
+
+#Create a file object from the file
+file_object = open(file_name, 'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
 #Pretend we read one line of data from the Sara file
-lineString = '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
+lineString = line_list[100]
 
 #Split the string into a list of data items
 lineData = lineString.split()
 
 #Extract items in list into varibles
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
+record_id = lineData[0] # ARGOS tracking record
+obs_date = lineData[2] # Observation date
+obs_lc = lineData[4] # Observation location class
+obs_lat = lineData[6] # Observation Latitude
+obs_lon = lineData[7] #Observation Longitude
 
 #Print the Location of Sara
 print(f'Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}')
